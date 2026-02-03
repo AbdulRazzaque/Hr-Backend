b2b19b169965198f86d2ee5788eab7c93e03a118
mhaslairazzaque@gmail.com <mhaslairazzaque@gmail.com>
2026-02-03 14:54:52 +0300

    Refactor Rprenewalform schema and controller to replace visa fields with healthCard

--- model/Forms/Rprenewalform.js
+++ model/Forms/Rprenewalform.js
@@ -8,10 +8,8 @@ const rprenewalformSchema = new Schema({
     employeeId:{type:Schema.Types.ObjectId,ref:"NewEmployee"},
     date: { type: Date, require: true },
     //new visa
-    newVisaRequested: { type: String, require: true },
-    BusinessVisaRequested: { type: String, require: true },
-    TransferVisaRequested: { type: String, require: true },
-    NewRPRequested: { type: String, require: true },
+ 
+    healthCard: { type: String, require: true },
     RPRenewalRequested: { type: String, require: true },
     exitPermitRequested: { type: String, require: true },
     OthersRequested: { type: String, require: true },
